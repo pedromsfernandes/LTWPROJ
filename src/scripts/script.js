b190fc3ee59470e44f13b57038ac8b407764602f
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-12-05 19:22:02 +0000

    small fix

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -8,7 +8,7 @@ request.open('get', '../api/api_get_session.php', true)
 request.send()
 
 var session
-var list = document.querySelector("#stories")
+var list = document.querySelector("#list")
 
 if(list){
     var original = list.innerHTML
