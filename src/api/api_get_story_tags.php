cc3b361f89604280b4794cb4837be0c25c628eda
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-12-06 19:26:19 +0000

    upvoted html and improved a lot of stuff

--- src/api/api_get_story_tags.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once('../database/db_post.php');
-
-    $story_id = $_POST['story_id'];
-
-    $tags = getStoryTags($story_id);
-
-    echo json_encode($tags);
-?>
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,10 @@
 'use strict'
 
-let inputs = document.querySelectorAll("#sorting input")
-
-let request = new XMLHttpRequest()
-request.addEventListener('load', getSession)
-request.open('get', '../api/api_get_session.php', true)
-request.send()
-
 var session
+getSession()
+
 var list = document.querySelector("#list")
+let inputs = document.querySelectorAll("#sorting input")
 
 if(list){
     var original = list.innerHTML
@@ -18,25 +14,34 @@ if(list){
 
 
 
+//---------
+//functions
+//---------
+
 function drawTopStories(event){
     event.preventDefault()
 
     let request = new XMLHttpRequest()
     request.addEventListener('load', handler)
-    request.open('get', '../api/api_get_stories.php', true)
+    request.open('get', '../api/api_get_top_stories.php', true)
     request.send()
 }
 
 function drawNewStories(event){
     event.preventDefault()
 
-    list.innerHTML = original
+    let request = new XMLHttpRequest()
+    request.addEventListener('load', handler)
+    request.open('get', '../api/api_get_new_stories.php', true)
+    request.send()
 }
 
 function handler(event){
     event.preventDefault()
 
     let newStories = JSON.parse(this.responseText)
+
+    console.log(newStories)
 
     let stories = list.querySelectorAll("div")
 
@@ -45,57 +50,68 @@ function handler(event){
     })
 
     newStories.forEach(function(data){
-        let story = document.createElement("article")
-        story.classList.add("story")
+        let story = document.createElement("div")
+        story.classList.add("titles")
 
-        story.innerHTML = `
-        <div class="titles">
-        <header><a href="../pages/story.php?id=`+data.post_id+`">`+data.post_title+`</a></header>
-        <ul>
-        `+getTags(data.tags)+
-        `
-        </ul>
-        <footer>Submitted by: `+data.user_name+` on `+data.post_date+` to <a href="../pages/channel.php?id=`+data.channel_id+`">`+data.channel+`</a></footer>
-        <div class="voteup">
-        <form method="post" action="../actions/action_vote.php">
-        <button name="upvote" type="submit"> <i class="fas fa-chevron-up"></i> </button>
-        <input type="hidden" name="post_op" value="`+data.post_op+`>">
-        <input type="hidden" name="post_id" value="`+data.post_id+`">
-        <input type="hidden" name="type" value="upvote">
-        <input type="hidden" name="csrf" value="`+session.csrf+`">
-        </form>
-        </div>
-        <div class = "votedown">
-        <form method="post" action="../actions/action_vote.php">
-        <button name="downvote" type="submit">  <i class="fas fa-chevron-down"></i> </button>
-        <input type="hidden" name="post_op" value="`+data.post_op+`">
-        <input type="hidden" name="post_id" value="`+data.post_id+`">
-        <input type="hidden" name="type" value="downvote">
-        <input type="hidden" name="csrf" value="`+session.csrf+`">
-        </form>
-        </div>
-
-        <p>`+data.num_votes+`</p>
-        </div>`
+        story.innerHTML = 
+        `   <div class="flex-container-1">
+                <div style= "order: 4" class="title">
+                    <header><a href="../pages/story.php?id=`+data.post_id+`">`+data.post_title+`</a></header>
+                </div>
+                <div style= "order: 1" class="vote">
+                    <form method="post" action="../actions/action_vote.php">
+                        <button name="upvote" type="submit"> <i class="fas fa-chevron-up"></i> </button>
+                        <input type="hidden" name="post_op" value="`+data.post_op+`">
+                        <input type="hidden" name="post_id" value="`+data.post_id+`">
+                        <input type="hidden" name="type" value="upvote">
+                        <input type="hidden" name="csrf" value="`+session.csrf+`">
+                    </form>
+                </div>
+                <div style= "order: 3" class = "vote">
+                    <form method="post" action="../actions/action_vote.php">
+                        <button name="downvote" type="submit">  <i class="fas fa-chevron-down"></i> </button>
+                        <input type="hidden" name="post_op" value="`+data.post_op+`">
+                        <input type="hidden" name="post_id" value="`+data.post_id+`">
+                        <input type="hidden" name="type" value="downvote">
+                        <input type="hidden" name="csrf" value="`+session.csrf+`">
+                    </form>
+                </div>
+                <div style= "order: 2" class="vote-amount">
+                    <p>`+data.num_votes+`</p>
+                </div>
+            </div>
+            <div class="flex-container-2">
+                <ul>
+            `       +getTags(data.tags)+`
+                </ul>
+                <footer>Submitted by: `+data.user_name+` on `+data.post_date+` to <a href="../pages/channel.php?id=`+data.channel_id+`">`+data.channel+`</a></footer>
+            </div>`
 
         list.append(story)
     })
+}
+
+function getSession(){
+    let request = new XMLHttpRequest()
+    request.addEventListener('load', sessionHandler)
+    request.open('get', '../api/api_get_session.php', true)
+    request.send()
+}
+
+function sessionHandler(event){
+    event.preventDefault()
+
+    session = JSON.parse(this.responseText)
 }
 
 function getTags(tags){
     let string = ""
 
     tags.forEach(function(tag){
-        string += "<li>"+tag.tag_text+"</li>\n"
+        string += `<li><a href="search.php?search_text=%23`+tag.tag_text+`&search_type=stories&submit=Search">`+tag.tag_text+`</a></li>\n`
     })
 
     return string
-}
-
-function getSession(event){
-    event.preventDefault()
-
-    session = JSON.parse(this.responseText)
 }
 
 function encodeForAjax(data) {
