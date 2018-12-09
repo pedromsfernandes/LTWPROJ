fc2053f94ff9efbcf8f3e930d8675811ee81f569
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2018-12-09 11:29:24 +0000

    Fixed issue w/ login.

--- src/includes/session.php
+++ src/includes/session.php
@@ -1,5 +1,5 @@
 <?php
-  session_set_cookie_params(0, '/', 'www.fe.up.pt', true, true);
+  //session_set_cookie_params(0, '/', 'www.fe.up.pt', true, true);
   ini_set('use_only_cookies', '1');
   ini_set('use_trans_sid', '0');
   session_start();
